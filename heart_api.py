ec399d8192521ef465219ade623dbfd9d77ea053
kwojtasi <wojtasikkonrad@gmail.com>
2018-05-05 23:06:16 +0200

    Enable CORS

--- heart_api.py
+++ heart_api.py
@@ -3,6 +3,24 @@ from bottle import Bottle, request, response
 from heart_model import HeartModel
 
 
+class EnableCors(object):
+    name = 'enable_cors'
+    api = 2
+
+    def apply(self, fn, context):
+        def _enable_cors(*args, **kwargs):
+            # set CORS headers
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+            if request.method != 'OPTIONS':
+                # actual request; reply with the actual response
+                return fn(*args, **kwargs)
+
+        return _enable_cors
+
+
 class HeartApp:
     def __init__(self, host, port):
         self._host = host
@@ -29,4 +47,5 @@ class HeartApp:
 
 if __name__ == '__main__':
     app = HeartApp(host='localhost', port=8080)
+    app._app.install(EnableCors())
     app.start()
